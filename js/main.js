110fa95bc745aacfd55b1b5ce176b70f38325576
John Stratton <jstrat0407@gmail.com>
2018-12-13 00:38:15 -0500

    cleared main.js

--- js/main.js
+++
@@ -1,8 +0,0 @@
-const submit = document.getElementById('submit');
-
-submit.addEventListener("click", () => {
-    const worksheet = document.getElementById('worksheet').files;
-    console.log(worksheet);
-    const workbook = XLSX.utils.sheet_to_html(worksheet);
-    console.log(workbook);
-});
