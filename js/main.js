ae7551ff1cb7d56e04340bbf7084b041f75cb7e1
John Stratton <jstrat0407@gmail.com>
2018-12-13 01:01:30 -0500

    reference e.target in place of 'worksheet'
    makes function reusable

--- js/main.js
+++ js/main.js
@@ -1,5 +1,5 @@
 const worksheet = document.getElementById('worksheet');
 function handleFile(e) {
-    console.log(worksheet.files[0].name);
+    console.log(e.target.files[0].name);
 }
 worksheet.addEventListener('change', handleFile);
