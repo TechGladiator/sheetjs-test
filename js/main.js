a2b428289fffa7bdf789c16f438123d748af64aa
John Stratton <jstrat0407@gmail.com>
2018-12-13 01:13:08 -0500

    add XLSX as X and xlsxworker as XW
    define SheetJS objects

--- js/main.js
+++ js/main.js
@@ -1,3 +1,13 @@
+/*global Uint8Array, Uint16Array, ArrayBuffer */
+/*global XLSX */
+const X = XLSX;
+const XW = {
+	/* worker message */
+	msg: 'xlsx',
+	/* worker scripts */
+	worker: './js/xlsxworker.js'
+};
+
 const worksheet = document.getElementById('worksheet');
 function handleFile(e) {
     console.log(e.target.files[0].name);
