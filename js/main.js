9e092d489a9aefdc6a091dbc44395d7a180ac57d
John Stratton <jstrat0407@gmail.com>
2018-12-13 20:35:23 -0500

    added event listener to file input

--- js/main.js
+++ js/main.js
@@ -9,10 +9,15 @@ const XW = {
 };
 
 const fileDrop = document.getElementById('file-drop');
+const fileInput = document.getElementById('file-input');
 
 // drop handler - not working
 fileDrop.addEventListener('drop', e => {
 	e.stopPropagation();
 	e.preventDefault();
 	e.dataTransfer.files;
+});
+
+fileInput.addEventListener('change', e => {
+	console.log(e.target.files[0].name);
 });
