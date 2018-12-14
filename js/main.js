0ca64b0d1357db4dcc044e5c229db34d62ce5ae1
John Stratton <jstrat0407@gmail.com>
2018-12-14 01:33:08 -0500

    added format output methods

--- js/main.js
+++ js/main.js
@@ -17,16 +17,71 @@ function processWB(wb) {
 
 	function getFormat() {
 		const formats = document.getElementsByName('format');
-		return () => {
-			for (let i = 0; i < formats.length; i++) {
-				if (formats[i].checked || formats.length === 1) {
-					return formats[i].nodeValue;
-				}
+		for (let i = 0; i < formats.length; i++) {
+			if (formats[i].checked || formats.length === 1) {
+				return formats[i].value;
 			}
 		}
 	}
 
-	OUT.innerText = JSON.stringify(wb);
+	function to_csv(workbook) {
+		const result = [];
+		workbook.SheetNames.forEach(sheetName => {
+			const csv = X.utils.sheet_to_csv(workbook.Sheets[sheetName]);
+			if(csv.length){
+				result.push(`SHEET: ${sheetName}`);
+				result.push("");
+				result.push(csv);
+			}
+		});
+		return result.join("\n");
+	}
+
+	function to_json(workbook) {
+		const result = {};
+		workbook.SheetNames.forEach(sheetName => {
+			const roa = X.utils.sheet_to_json(workbook.Sheets[sheetName], {header:1});
+			if (roa.length) {
+				result[sheetName] = roa;
+			}
+		});
+		return JSON.stringify(result, 2, 2);
+	}
+
+	function to_fmla(workbook) {
+		const result = [];
+		workbook.SheetNames.forEach(sheetName => {
+			const formulae = X.utils.get_formulae(workbook.Sheets[sheetName]);
+			if (formulae.length) {
+				result.push(`SHEET: ${sheetName}`);
+				result.push("");
+				result.push(formulae.join("\n"));
+			}
+		});
+		return result.join("\n");
+	}
+
+	function to_html(workbook) {
+		HTMLOUT.innerHTML = "";
+		workbook.SheetNames.forEach(sheetName => {
+			const htmlstr = X.write(workbook, {sheet:sheetName, type:'string', bookType:'html'});
+			HTMLOUT.innerHTML += htmlstr;
+		});
+		return "";
+	}
+
+	let output = "";
+	switch(getFormat()) {
+		case "form": output = to_fmla(wb); break;
+		case "html": output = to_html(wb); break;
+		case "json": output = to_json(wb); break;
+		default: output = to_csv(wb);
+	}
+	if (OUT.innerText === undefined) {
+		OUT.textContent = output;
+	} else {
+		OUT.innerText = output;
+	}
 }
 
 function processFile(files) {
