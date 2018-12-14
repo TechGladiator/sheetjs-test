8bdf4972b51c805b7fda94123b222fbd5f49913e
John Stratton <jstrat0407@gmail.com>
2018-12-13 19:54:59 -0500

    removed code

--- js/main.js
+++ js/main.js
@@ -8,21 +8,3 @@ const XW = {
 	worker: './js/xlsxworker.js'
 };
 
-const worksheet = document.getElementById('worksheet');
-function handleFile(e) {
-	const f = e.target.files[0];
-	console.log(f.name);
-	let rABS = typeof FileReader !== "undefined" && (FileReader.prototype||{}).readAsBinaryString;
-	let use_worker = typeof Worker !== 'undefined';
-	const reader = new FileReader();
-	reader.onload = g => {
-		if(typeof console !== 'undefined') {
-			console.log("onload", new Date(), rABS, use_worker);
-		}
-		const worker = new Worker(XW.worker);
-		worker.onmessage = h => {
-			console.log(h);
-		}
-	}
-}
-worksheet.addEventListener('change', handleFile);
